9f95b8ac999ab3f58c46ff50493505409751f2c2
Nozimjon <73873455+Nozimjon2023@users.noreply.github.com>
2023-04-14 10:05:38 +0500

    Create

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Nozimjon2023/GolangCharts
+
+go 1.17
+
+require github.com/robfig/cron/v3 v3.0.1
--- main.go
+++ main.go
@@ -1,21 +1,8 @@
 package main
 
 import (
-	"github.com/Nozimjon2023/GolangCharts"
-	"os"
-	"os/signal"
-	"syscall"
-	"github.com/robfig/cron/v3"
+	"github.com/Nozimjon2023/GolangCharts/pkg"
 )
-
 func main() { //точка входа модуля
-  // pkg.Run()
-  cron := cron.New()
-  cron.AddFunc("@hourly", pkg.Run)// @hourly pkg.Run() вызывается каждый час
-  go cron.Start()
-  defer cron.Stop()
-  sig := make(chan os.Signal, 1)
-  signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
-  <-sig
-  
+ RUN()
 }
